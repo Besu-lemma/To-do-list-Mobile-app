697dc3cbbeb1e084df33bd4000207b4fca97a478
Besu-lemma <besufikadle95@gmail.com>
2025-03-15 16:23:19 +0300

    search bar and the to do list is created

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'To do list',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
       home: Home(),
     );
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mini_project/constraints/colors.dart';
+import '../widgets/todo_item.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -6,11 +8,95 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Todo app"),
-      ),
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
       body: Container(
-        child: Text("this is home screen"),
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        child: Column(
+          children: [
+            searchBox(),
+            Expanded(child: 
+            ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 50,
+                    bottom: 20
+                  ),
+                  child: Text("All Todos",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  
+                  ),
+                  ),
+                ),
+                TodoItem(),
+                TodoItem(),
+                TodoItem(),
+                TodoItem(),
+                TodoItem(),
+                TodoItem(),
+                TodoItem(),
+              ],
+            )
+            )
+          ],
+    )
+      )
+      );
+  }
+
+Widget searchBox(){
+  return Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20)
+
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(0),
+                  prefixIcon: Icon(
+                    Icons.search,
+                  color: tdBlack,
+                  size: 20,
+                  ),
+                  prefixIconConstraints: BoxConstraints(
+                    maxHeight: 20,
+                    minWidth: 25
+                  ),
+                  border: InputBorder.none,
+                  hintText: "Search here"
+                ),
+              ),
+  );
+           
+}
+
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Icon(
+          Icons.menu,
+          color: tdBlack,
+          size: 30,
+        ),
+        SizedBox(
+         height: 40,
+         width: 40,
+         child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.asset('assets/images/avatar.jpg'),
+         ),
+        )
+      ],
       ),
     );
   }
